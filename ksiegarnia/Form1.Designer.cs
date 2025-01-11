a82caa18b3ce20628383355cb21cbbbb1376cf88
Jacek Ragan <rjacek4@gmail.com>
2025-01-11 17:29:03 +0100

    Dodaj pliki projektów.

---
+++ ksiegarnia/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ksiegarnia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(12, 34);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(399, 277);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numer
+            // 
+            this.numer.Location = new System.Drawing.Point(186, 344);
+            this.numer.Name = "numer";
+            this.numer.ShortcutsEnabled = false;
+            this.numer.Size = new System.Drawing.Size(34, 20);
+            this.numer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wpisz numer ksiazki";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Wypozycz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Zwroc ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(163, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Odswiez";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 470);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 442);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Dodaj ksiazke do systemu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 473);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "tytul";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 503);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 506);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "autor";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(152, 529);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 27);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Dodaj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 570);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numer);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Ksiegarnia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox numer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ksiegarnia/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.BC;
+namespace ksiegarnia
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connect();
+        }
+        private void connect()
+        {
+            string connect = "wpisz swoj serwer :p";
+            connection = new MySqlConnection(connect);
+            {
+                try
+                {
+                   
+                    connection.Open();
+                    refresh();
+                }         
+                catch(Exception ex) 
+                { 
+                      MessageBox.Show("nie udalo sie xd" + ex.Message);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nwm = numer.Text;
+            int id = Convert.ToInt32(nwm);
+            string sql2 = $"UPDATE ksiazka SET wypozyczona = true WHERE id = {id};";
+            MySqlCommand query = new MySqlCommand(sql2, connection);
+            MySqlDataReader reader = query.ExecuteReader();
+            MessageBox.Show("Twoja ksiazka zostala wypozyczona");
+            reader.Close();
+            refresh();
+        }
+        private void refresh()
+        {
+            string sql = "Select * from ksiazka";
+            MySqlCommand query = new MySqlCommand(sql, connection);
+            MySqlDataReader reader = query.ExecuteReader();
+            if (reader.HasRows)
+            {
+                StringBuilder ksiazka = new StringBuilder();
+                while (reader.Read())
+                {
+                    // Pobieranie informacji o książce
+                    int id = reader.GetInt32(3);
+                    string tytul = reader.GetString(0);
+                    string autor = reader.GetString(2);
+                    bool wyp = reader.GetBoolean(1);
+
+                    // Dodawanie informacji o książce do StringBuildera
+                    ksiazka.AppendLine($"{id}. ,,{tytul}'' {autor} - {(wyp ? "Wypożyczona" : "Wolna")}");
+                }
+                textBox2.Text = ksiazka.ToString();
+                reader.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nwm = numer.Text;
+            int id = Convert.ToInt32(nwm);
+            string sql2 = $"UPDATE ksiazka SET wypozyczona = false WHERE id = {id};";
+            MySqlCommand query = new MySqlCommand(sql2, connection);
+            MySqlDataReader reader = query.ExecuteReader();
+            MessageBox.Show("Zwrociles ksiazke :)");
+            reader.Close();
+            refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string tytul = textBox1.Text;
+            string autor = textBox3.Text;
+            if(tytul.Length == 0 || autor.Length == 0)
+            {
+                MessageBox.Show("Nie mozesz dodac pustej ksiazki");
+            }
+            else
+            {
+                string sql3 = $"INSERT INTO ksiazka (nazwa,autor) VALUES ('{tytul}','{autor}');";
+                MySqlCommand query = new MySqlCommand( sql3, connection);
+                MySqlDataReader reader = query.ExecuteReader();
+                MessageBox.Show("Dodales ksiazke do systemu");
+                reader.Close();
+                refresh();   
+            }
+        }
+    }
+}
